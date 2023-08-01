e303149ce523d52722cb62e85a3a4c177004fad4
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-08-01 23:02:41 +0200

    feat: support for x3 images and event ticket type

--- src/apple/apple.service.ts
+++ src/apple/apple.service.ts
@@ -15,12 +15,18 @@ export class AppleService {
     const certs = this.loadCerts()
     const pass = new PKPass({}, certs, this.getPassProps(template, userId))
 
-    pass.type = 'generic'
+    pass.type = 'eventTicket'
 
     pass.primaryFields.push({
       key: 'name',
       label: 'Név',
       value: name,
+    })
+
+    pass.secondaryFields.push({
+      key: 'eventName',
+      label: 'Esemény',
+      value: template.name,
     })
 
     pass.setBarcodes({
@@ -31,8 +37,21 @@ export class AppleService {
     const identifier = generateRandomString()
     const passFileName = `${identifier}.pkpass`
     try {
-      pass.addBuffer('logo.png', fs.readFileSync(path.resolve(__dirname, '../../templates', template.id, 'logo.png')))
-      pass.addBuffer('icon.png', fs.readFileSync(path.resolve(__dirname, '../../templates', template.id, 'icon.png')))
+      const logoUrl = path.resolve(__dirname, '../../templates', template.id, 'logo.png')
+      const logo2Url = path.resolve(__dirname, '../../templates', template.id, 'logo@2x.png')
+      const iconUrl = path.resolve(__dirname, '../../templates', template.id, 'icon.png')
+      const icon2Url = path.resolve(__dirname, '../../templates', template.id, 'icon@2x.png')
+
+      pass.addBuffer('logo.png', fs.readFileSync(logoUrl))
+      pass.addBuffer('icon.png', fs.readFileSync(iconUrl))
+
+      if (fs.existsSync(logo2Url)) {
+        pass.addBuffer('logo@2x.png', fs.readFileSync(logo2Url))
+      }
+
+      if (fs.existsSync(icon2Url)) {
+        pass.addBuffer('icon@2x.png', fs.readFileSync(icon2Url))
+      }
       const passPath = await this.writePassToFile(pass, passFileName)
       const passFile = fs.readFileSync(passPath)
       fs.rmSync(passPath)
@@ -72,7 +91,6 @@ export class AppleService {
       teamIdentifier: TEAM_ID,
       organizationName: ORG_NAME,
       description: template.name,
-      logoText: template.name,
       foregroundColor: template.foregroundColor,
       labelColor: template.labelColor,
       backgroundColor: template.backgroundColor,
