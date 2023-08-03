b5e2d03317bdc99305157d9945279cfd0ef60624
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-08-03 22:25:12 +0200

    feat: hi and info endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,26 @@
 import { Controller, Get } from '@nestjs/common'
 
 import { AppService } from './app.service'
+import { TemplateService } from './template/template.service'
+import { ORG_NAME } from './utils/configuration'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly templateService: TemplateService
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello()
+  @Get('hi')
+  getHi() {
+    return 'Hi!'
+  }
+
+  @Get('info')
+  getInfo() {
+    return {
+      templates: this.templateService.getTemplates(),
+      organizationName: ORG_NAME,
+    }
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common'
 
+import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AppleModule } from './apple/apple.module'
 import { ContentDeliveryModule } from './content-delivery/content-delivery.module'
 import { GoogleModule } from './google/google.module'
+import { TemplateModule } from './template/template.module'
 
 @Module({
-  imports: [AppleModule, GoogleModule, ContentDeliveryModule],
+  controllers: [AppController],
+  imports: [AppleModule, GoogleModule, ContentDeliveryModule, TemplateModule],
   providers: [AppService],
 })
 export class AppModule {}
